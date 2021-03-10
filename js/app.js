2c1f0c945d824d9bdcbf992739b613481534a2db
Pablo Benedix <pabling.benedix@gmail.com>
2021-03-10 11:43:24 -0300

    Social Media Dashboard con boton terminado

--- js/app.js
+++ js/app.js
@@ -6,5 +6,11 @@ if (window.matchMedia('(prefers-color-scheme: dark)')) {
 }
 
 checkbox.addEventListener('change', function(event) {
-        document.body.classList.toggle('is-dark-mode')
+    if(this.checked) {
+        document.body.classList.remove('is-light-mode');
+        document.body.classList.add('is-dark-mode');
+    } else {
+        document.body.classList.remove('is-dark-mode');
+        document.body.classList.add('is-light-mode');
+    }
 })
